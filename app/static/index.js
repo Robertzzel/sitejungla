c5680a1b9eacb855e9d26f583f77aa7adb976ff5
Tutuianu Robert <robertzzel_2001@yahoo.com>
2020-06-04 20:25:48 +0300

    Adaugat vector cu raspunsuri

--- app/static/index.js
+++ app/static/index.js
@@ -15,7 +15,7 @@ var intrebari=[
 
     'pare ca ai nevoie de ceva dulce'
 ];
-var raspunsuri=[];
+var raspunsuri=['0','1','2','3','4','5','6','7'];
 var btn= document.querySelector('#btn');
 var msg= document.querySelector('h3');
 var inp= document.querySelector('input');
@@ -35,50 +35,50 @@ btn.addEventListener('click',function(){
     rasp= inp.value;
 
     //intrbarea 0
-    if(rasp == "12" && i0==0)
+    if(rasp == raspunsuri[0] && i0==0)
     {
         i0=1;
         msg.innerHTML=intrebari[0];
     }
 
     //intrbarea 1
-    if(rasp == "34" && i1==0)
+    if(rasp == raspunsuri[1] && i1==0)
     {
         i1=1;
         msg.innerHTML=intrebari[1];
     }
     //intrbarea 2
-    if(rasp == "56" && i2==0)
+    if(rasp == raspunsuri[2] && i2==0)
     {
         i2=1;
         msg.innerHTML=intrebari[2];
     }
     //intrbarea 3
-    if(rasp == "78" && i3==0)
+    if(rasp == raspunsuri[3] && i3==0)
     {
         i3=1;
         msg.innerHTML=intrebari[3];
     }
     //intrbarea 4
-    if(rasp == "910" && i4==0)
+    if(rasp == raspunsuri[4] && i4==0)
     {
         i4=1;
         msg.innerHTML=intrebari[4];
     }
     //intrbarea 5
-    if(rasp == "1112" && i5==0)
+    if(rasp == raspunsuri[5] && i5==0)
     {
         i5=1;
         msg.innerHTML=intrebari[5];
     }
     //intrbarea 6
-    if(rasp == "1314" && i6==0)
+    if(rasp == raspunsuri[6] && i6==0)
     {
         i6=1;
         msg.innerHTML=intrebari[6];
     }
     //intrbarea 7
-    if(rasp == "1516" && i7==0)
+    if(rasp == raspunsuri[7] && i7==0)
     {
         i7=1;
         msg.innerHTML=intrebari[7];
