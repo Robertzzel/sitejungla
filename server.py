8ab4d0b791b91692c0ed111ecc7257557431a387
unknown <robertzel_2001@yahoo.com>
2020-06-03 17:38:26 -0700

    First commit

--- server.py
+++ server.py
@@ -3,12 +3,14 @@ from flask import Flask, render_template , request, redirect, url_for
 
 app=Flask(__name__)
 
+
 @app.route('/',methods=['POST','GET'])
 def home():
     if request.method == "GET":
         return render_template('start.html')
     elif request.method=="POST":
         return redirect(url_for('start'))
+
 
 @app.route('/start',methods=['POST','GET'])
 def start():
